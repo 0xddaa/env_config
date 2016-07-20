912066b67d884363dec90b1b9d7670d0f5b5210c
0xddaa <0xddaa@gmail.com>
2016-07-20 12:41:11 +0000

    remove change dir back

--- setup/link.sh
+++ setup/link.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-echo "[*] Change directory to ~/env_config/config..."
 cd ~/env_config/config 2>&1 > /dev/null
 RCFILES="bashrc cshrc gdbinit gitconfig screenrc vimrc"
 
 for f in $RCFILES; do
     DEST=~/.$f
+    echo "[*] link $DEST ..."
 	[ -f "$DEST" ] && rm -f "$DEST"
 	ln -s ~/env_config/config/$f "$DEST"
 done
-echo "[*] Change back"
-cd - 2>&1 > /dev/null
--- setup/unlink.sh
+++ setup/unlink.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "[*] Change directory to ~/env_config/config..."
-cd ~/env_config/config 2>&1 > /dev/null
 RCFILES="bashrc cshrc gdbinit gitconfig screenrc vimrc"
 
 for f in $RCFILES; do
     DEST=~/.$f
+    echo "[*] unlink $DEST ..."
 	[ -L "$DEST" ] && unlink "$DEST"
 done
-echo "[*] Change back"
-cd - 2>&1 > /dev/null
