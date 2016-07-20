973c6f19733f122423728c0ea9000606118c037c
0xddaa <0xddaa@gmail.com>
2016-07-20 11:56:07 +0000

    sperate directory

--- setup/unlink.sh
+++ setup/unlink.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "[*] Change directory to ~/env_config/config..."
+cd ~/env_config/config 2>&1 > /dev/null
 RCFILES="bashrc cshrc gdbinit gitconfig screenrc vimrc"
 
 for f in $RCFILES; do
     DEST=~/.$f
 	[ -L "$DEST" ] && unlink "$DEST"
 done
+echo "[*] Change back"
+cd - 2>&1 > /dev/null
