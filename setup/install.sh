6bab38e6a15e291cfe06704eabc4d1a62c796b89
0xddaa <0xddaa@gmail.com>
2016-07-20 22:07:51 +0800

    fix install script bug & check executed by root

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # test on ubuntu 14.04 trusty
 
+if [ "$EUID" -ne 0 ]; then
+    echo "please run as root."
+    exit
+fi
+
 apt-get -y update
 apt-get -y upgrade
 
@@ -8,10 +13,15 @@ apt-get -y upgrade
 apt-get -y install git nmap
 
 # complie & library
-apt-get -y install gcc g++ nasm libc-dev:i386 libssl-dev libssl-dev:i386 gcc-multilib g++-multilib libc6-dev-i386
+if [ "$(uname -i)" == "x86_64" ]; then
+    apt-get -y install gcc g++ nasm libssl-dev libffi-dev gcc-multilib g++-multilib
+    apt-get -y install libc-dev:i386 libssl-dev:i386 libc6-dev-i386 libffi-dev:i386
+elif [ "$(uname -i)" == "aarch64" ]; then
+    apt-get -y install gcc g++ nasm libssl-dev libffi-dev
+fi
 
 # python package
-apt-get -y install python-dev
+apt-get -y install python-dev ipython
 wget -O- https://bootstrap.pypa.io/get-pip.py | python
 pip install pwntools
 pip install requests 
