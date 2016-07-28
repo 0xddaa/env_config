42c7b92c26bf145b409d42b61a3c0ab3635b187e
0xddaa <0xddaa@gmail.com>
2016-07-29 01:22:47 +0800

    fix indent

--- setup/unlink.sh
+++ setup/unlink.sh
@@ -5,5 +5,5 @@ RCFILES="bashrc cshrc gdbinit gitconfig screenrc vimrc"
 for f in $RCFILES; do
     DEST=~/.$f
     echo "[*] unlink $DEST ..."
-	[ -L "$DEST" ] && unlink "$DEST"
+    [ -L "$DEST" ] && unlink "$DEST"
 done
