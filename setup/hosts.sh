9becac9655def300dcc855d087d8112fae9e78bd
0xddaa <0xddaa@gmail.com>
2017-04-18 03:19:51 +0800

    add pwntw

--- setup/hosts.sh
+++ setup/hosts.sh
@@ -7,5 +7,5 @@ fi
 
 echo "140.113.209.3   sqduty" >> /etc/hosts
 echo "143.248.249.64  pwnkr" >> /etc/hosts
-echo "104.199.161.89  gcp" >> /etc/hosts
+echo "139.162.123.119 pwntw" >> /etc/hosts
 echo "127.0.0.1       localhost $(hostname)" >> /etc/hosts
