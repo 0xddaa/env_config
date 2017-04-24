514f0d61b83933284dd1d77108bf29b5bc6bdd08
0xddaa <0xddaa@gmail.com>
2017-04-25 00:41:19 +0800

    use pwndbg instead of peda

--- setup/tool.sh
+++ setup/tool.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 mkdir -p ~/tools
-git clone http://github.com/longld/peda.git ~/tools/peda
-git clone http://github.com/scwuaptx/Pwngdb ~/tools/Pwngdb
 git clone http://github.com/0xddaa/ctfcode ~/code
+
+# gdb
+git clone http://github.com/scwuaptx/Pwngdb ~/tools/Pwngdb
+git clone https://github.com/0xddaa/pwndbg ~/tools/pwndbg
+pushd ~/tools/pwndbg
+./setup.sh
+popd
